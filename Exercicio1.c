446bc56e4af27fd1fbe0d4fbfcd1fbd2495f5d4c
Carla <86434863+CarlaChicareli@users.noreply.github.com>
2022-05-14 22:46:25 -0300

    Update Exercicio1.c

--- Exercicio1.c
+++ Exercicio1.c
@@ -1,13 +1,7 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 //Carla Heloisa e Carolina Fonseca (11/05) 
-//LPI exercício 1
-
+//LPI exercício  1
 #include <stdio.h>
 
 
